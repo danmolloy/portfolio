e3bbc9a2817b2c0ae914430a5afde2c5b7aa1483
Daniel Molloy <danielmolloy_6@icloud.com>
2021-09-13 18:27:37 +1000

    tests added

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,48 @@
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
+import { unmountComponentAtNode } from 'react-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let container = null
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
 });
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+
+it("Welcome component renders", () => {
+  act(() => {
+    render(<App />, container)
+  })
+  const welcomeText = screen.getByText(/I'm Dan/i);
+  expect(welcomeText).toBeInTheDocument();
+})
+
+it("About component renders", () => {
+  act(() => {
+    render(<App />, container)
+  })
+  const aboutText = screen.getByText(/About/i);
+  expect(aboutText).toBeInTheDocument();
+})
+
+it("Portfolio component renders", () => {
+  act(() => {
+    render(<App />, container)
+  })
+  const portfolioText = screen.getByText(/portfolio/i);
+  expect(portfolioText).toBeInTheDocument();
+})
+
+it("Contact component renders", () => {
+  act(() => {
+    render(<App />, container)
+  })
+  cont contactText = screen.getByText(/contact/i);
+  expect(contactText).toBeInTheDocument();
+})
