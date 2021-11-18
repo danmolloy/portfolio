169607cfede1db5d334bc4e78c2e6abe05410632
Daniel Molloy <danielmolloy_6@icloud.com>
2021-11-18 09:43:16 +0000

    img bug fix

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,16 +4,16 @@ const projects = [
    {
     title: "Twitter Clone",
     image: "logo192.png",
-    blurb: "A full stack social network, complete with authentication, authorization, notifications and direct messaging. Share your thoughts with your friends.",
-    link: "https://www.google.com",
+    blurb: "A full stack social network, complete with authentication, authorization, notifications and direct messaging. Share your thoughts with your friends and interact with them.",
+    link: "https://sharp-hugle-251ecd.netlify.app/",
     code: "https://github.com/danmolloy/twitter-clone",
     stack: ["React", "Apollo Client", "TypeScript", "Tailwind", "Jest", "React Router", "Node.js", "Apollo Server", "Prisma", "SQLIte",]
 
   },
   {
     title: "Battleships",
-    image: "/img/battleships.png",
-    blurb: "Play the classic board game against the computer. Try and beat the highscores!",
+    image: "%PUBLIC_URL%/img/battleships.png",
+    blurb: "Play the classic board game against the computer. Go face-to-face with the computer to see who will win the battle. Try and beat the highscores!",
     link: "https://mystifying-saha-d28822.netlify.app/",
     code: "https://github.com/danmolloy/battleships",
     stack: ["React", "Redux", "Tailwind", "Typescript", "Jest", "Mirage JS"]
