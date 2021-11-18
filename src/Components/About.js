640533f7a50d7f2ac441b3a1fb9cdc5fe688cc1f
danmolloy <64697812+danmolloy@users.noreply.github.com>
2021-11-18 12:14:39 +0000

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,10 +1,12 @@
+import Portrait from "../images/bass.jpeg"
+
 export const About = () => {
   return (
 
     <div id="about" className="text-black h-auto flex flex-col text-center justify-center my-24 mx-12">
       <h1 className="p-4 text-4xl">About</h1>
       <div className="flex flex-col itmes-center md:items-start md:flex-row justify-center ">
-        <img src={require("../images/bass.jpeg")} className="h-auto w-auto max-w-sm" />
+        <img src={Portrait} className="h-auto w-auto max-w-sm" />
         <div className="md:w-1/2 text-left md:ml-4">
           <div className="p-2">
             <p>Originally from Australia, I moved to London in 2014.</p>
