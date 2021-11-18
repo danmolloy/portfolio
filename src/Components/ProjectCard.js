297e54fee87e98e67dd2f56e8906fa1e2e118372
Daniel Molloy <danielmolloy_6@icloud.com>
2021-11-18 14:51:21 +0000

    rel added to links

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -3,15 +3,15 @@ export const ProjectCard = (props) => {
     <div className="project-card bg-white flex flex-col border m-8 w-auto items-center shadow">
       <h2 className="text-3xl mt-2">{props.project.title}</h2>
       <div className="flex flex-col md:flex-row items-center md:items-start md:justify-evenly m-4">
-      <a href={props.project.link}>
+      <a href={props.project.link} target="_blank" rel="noreferrer">
       <img src={props.project.image} alt="Project preview" className="w-80 border h-auto md:mr-2"/>
       </a>
       <div className="flex flex-col lg:flex-row">
       <div className="flex flex-col md:ml-2" >
         <p className="w-72">{props.project.blurb}</p>
         <div className="flex flex-col">
-          <a href={props.project.link}target="_blank">Live Example</a>
-          <a href={props.project.code} target="_blank">Code</a>
+          <a href={props.project.link}target="_blank" rel="noreferrer">Live Example</a>
+          <a href={props.project.code} target="_blank" rel="noreferrer">Code</a>
         </div>
       </div>
       <div className="hidden md:flex flex-col ml-2 text-gray-400">
