804de2675cc1b781f9ffa5ba7266f726c6fef53f
Daniel Molloy <danielmolloy_6@icloud.com>
2021-11-18 21:12:40 +0000

    styling

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -2,9 +2,9 @@ import WelcomePic from "../images/portrait.jpeg"
 
 export const Welcome = () => {
   return (
-    <div className="welcome flex flex-col items-center justify-center">
-      <img src={WelcomePic} alt="Dan in a garden" className="max-h-screen-sm max-w-screen-sm md:self-end md:mr-20"/>
-      <div className="absolute sm:-mt-96 text-center text-white lg:text-black md:left-20">
+    <div className="welcome flex flex-col items-center justify-center h-full">
+      <img src={WelcomePic} alt="Dan in a garden" className="sm:max-h-screen-sm sm:max-w-screen-sm md:self-end md:mr-20"/>
+      <div className="absolute top-12 md:top-20 md:left-20 text-center text-white lg:text-black welcome-text">
         <h1 className="text-5xl md:text-6xl font-semibold z-10 text">Dan Molloy</h1>
         <h2 className="text-3xl md:text-4xl z-10 text">Front end developer</h2>
       </div>
